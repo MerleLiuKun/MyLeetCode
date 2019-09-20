937f55c1d567f77e6544f8995afc65541816dd31
ikronskun <merle.liukun@gmail.com>
2019-09-20 19:06:12 +0800

    fix(error): fix

--- algorithms/Array/121-SellStock-i/py/sell_stok.py
+++ algorithms/Array/121-SellStock-i/py/sell_stok.py
@@ -19,7 +19,7 @@ class Solution:
         max_profit = 0
         tmp_profit = 0
         for idx in range(1, len(prices)):
-            tmp_profit = prices[idx] - prices[idx - 1]
+            tmp_profit += prices[idx] - prices[idx - 1]
             if tmp_profit > max_profit:
                 max_profit = tmp_profit
             elif tmp_profit < 0:
@@ -27,11 +27,12 @@ class Solution:
 
         return max_profit
 
+    # 2 的另一个版本
     def get_max_profit_3(self, prices):
-        last, max_profit = 0, 0
+        tmp_profit, max_profit = 0, 0
 
         for idx in range(1, len(prices)):
-            last = max(0, last + prices[idx] - prices[idx - 1])
-            max_profit = max(max_profit, last)
+            tmp_profit = max(0, tmp_profit + prices[idx] - prices[idx - 1])
+            max_profit = max(max_profit, tmp_profit)
         return max_profit
 
